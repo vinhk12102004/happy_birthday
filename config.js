e0806ae7083e319f3381ee21215b4b6e9fcf72d8
Trong Vinh Le <117925608+vinhk12102004@users.noreply.github.com>
2024-04-18 21:06:44 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '';
+const giftUrl = '';
+const eventName = 'Chúc mừng sinh nhật Hà';   
+const titleCard = 'Chúc mừng sinh nhật🍍🍍🍍';
+const contentCard = 'Nhân ngày sinh nhật, chúc cậu luôn thành công trong công việc học tập, thuận lợi trong các dự định và luôn hạnh phúc trong tình yêu và cuộc sống nhaa';
+
+const giftImage = 'Ha.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
